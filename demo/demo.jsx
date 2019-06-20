8fbd89cbe271a9cbcd39aec8704b51cd52458bc2
宛天 <zhaobin.zb@alibaba-inc.com>
2019-06-20 20:11:51 +0800

    update user-select:none;

--- demo/demo.jsx
+++ demo/demo.jsx
@@ -148,7 +148,7 @@ export default class Demo extends Component {
             this.imageMarkingRef = ref;
           }}
           dataSource={dataSource}
-          readOnly // 是否只读
+          readOnly={false} // 是否只读
           isDeleteConfirmOpen // 是否开启删除确认框
           deleteConfirm={deleteConfirm} // 删除确认框组件
           onChange={this.onChange} // 画布发生变化时的回调事件
